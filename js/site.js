26ba14d0953cb87fce5c19a878c6602e8b2108ce
Nate Cornelius <nate@holeintheroof.com>
2013-08-26 16:33:05 -0500

    Initial Commit for Limbo Version 2

--- js/site.js
+++ js/site.js
@@ -1,46 +1,25 @@
-/**
-* Bootstrap.js by @fat & @mdo
-* plugins: bootstrap-transition.js, bootstrap-collapse.js
-* Copyright 2012 Twitter, Inc.
-* http://www.apache.org/licenses/LICENSE-2.0.txt
-*/
-!function(a){a(function(){a.support.transition=function(){var a=function(){var a=document.createElement("bootstrap"),b={WebkitTransition:"webkitTransitionEnd",MozTransition:"transitionend",OTransition:"oTransitionEnd otransitionend",transition:"transitionend"},c;for(c in b)if(a.style[c]!==undefined)return b[c]}();return a&&{end:a}}()})}(window.jQuery),!function(a){var b=function(b,c){this.$element=a(b),this.options=a.extend({},a.fn.collapse.defaults,c),this.options.parent&&(this.$parent=a(this.options.parent)),this.options.toggle&&this.toggle()};b.prototype={constructor:b,dimension:function(){var a=this.$element.hasClass("width");return a?"width":"height"},show:function(){var b,c,d,e;if(this.transitioning||this.$element.hasClass("in"))return;b=this.dimension(),c=a.camelCase(["scroll",b].join("-")),d=this.$parent&&this.$parent.find("> .accordion-group > .in");if(d&&d.length){e=d.data("collapse");if(e&&e.transitioning)return;d.collapse("hide"),e||d.data("collapse",null)}this.$element[b](0),this.transition("addClass",a.Event("show"),"shown"),a.support.transition&&this.$element[b](this.$element[0][c])},hide:function(){var b;if(this.transitioning||!this.$element.hasClass("in"))return;b=this.dimension(),this.reset(this.$element[b]()),this.transition("removeClass",a.Event("hide"),"hidden"),this.$element[b](0)},reset:function(a){var b=this.dimension();return this.$element.removeClass("collapse")[b](a||"auto")[0].offsetWidth,this.$element[a!==null?"addClass":"removeClass"]("collapse"),this},transition:function(b,c,d){var e=this,f=function(){c.type=="show"&&e.reset(),e.transitioning=0,e.$element.trigger(d)};this.$element.trigger(c);if(c.isDefaultPrevented())return;this.transitioning=1,this.$element[b]("in"),a.support.transition&&this.$element.hasClass("collapse")?this.$element.one(a.support.transition.end,f):f()},toggle:function(){this[this.$element.hasClass("in")?"hide":"show"]()}};var c=a.fn.collapse;a.fn.collapse=function(c){return this.each(function(){var d=a(this),e=d.data("collapse"),f=a.extend({},a.fn.collapse.defaults,d.data(),typeof c=="object"&&c);e||d.data("collapse",e=new b(this,f)),typeof c=="string"&&e[c]()})},a.fn.collapse.defaults={toggle:!0},a.fn.collapse.Constructor=b,a.fn.collapse.noConflict=function(){return a.fn.collapse=c,this},a(document).on("click.collapse.data-api","[data-toggle=collapse]",function(b){var c=a(this),d,e=c.attr("data-target")||b.preventDefault()||(d=c.attr("href"))&&d.replace(/.*(?=#[^\s]+$)/,""),f=a(e).data("collapse")?"toggle":c.data();c[a(e).hasClass("in")?"addClass":"removeClass"]("collapsed"),a(e).collapse(f)})}(window.jQuery)
-
-function is_mobile(){
-	return ($('.navbar .btn-navbar').is(':visible'));
-}
-
-function check_hover_nav(){
-	$('.navbar').find('.up-arrow').remove();
-	if(is_mobile()){
-		$('.navbar').removeClass('hover-nav');
+/* Restyle Convergence Form Modules using Bootstrap */
+$(document).ready(function() {
+	$('.form-module, .form-comments').addClass('form-horizontal');
+	$('.form-horizontal .control-group, .form-comments .control-group').addClass('form-group');
+	$('.form-group select, .form-group textarea, .form-group input').addClass('form-control');
+	$('.comments, .field.field-tags').addClass('well');
 	
-		$('.navbar ul.menu-level-0 li.has-children').hover(			
-			function(){ return false; },
-			function(){ return false; }
-		);
-	}
-	else{
-		$('.navbar').addClass('hover-nav');
-		$('.navbar ul.menu-level-0 li.has-children ul.menu-level-1').prepend('<li class="up-arrow"></li>');
-		$('.navbar ul.menu-level-0 li.has-children').hover(			
-			function(){ $( this ).find('ul.menu-level-1').fadeIn(200); $(this).addClass('show');  },
-			function(){ $( this ).find('ul.menu-level-1').fadeOut(40); $(this).removeClass('show'); }
-		);
+	//Add Bootstrap Dropdowns to Nav
+	$('.nav li.has-children ul.child-menu').each(function() {
+		var copyThis = $(this).siblings('a');
+		copyThis.removeClass('active');
+		var copyClass = copyThis.attr('class');
+		console.log(copyClass);
 		
-	}
-	
-}
-
-$(document).ready(function(){
-	check_hover_nav();
-
-	//Dropdown Navigation//	
-	$(window).resize(check_hover_nav);
-		
+		$(this).parent().addClass('dropdown');
+		$(this).addClass('dropdown-menu');
+		copyThis.clone().prependTo(this).wrap('<li class="'+copyClass+'"><li>');
+		$(this).siblings().attr('data-toggle', 'dropdown');
+		/*Remove the following line to remove the arrows next to dropdown menus*/
+		$(this).siblings().append('<span class="caret"></span>');
+	});
 });
-
-
 
 
 /*!
