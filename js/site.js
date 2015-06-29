3ca2653e269c7cb1bb7535f48b8dd65ec170a694
websmith <nate@holeintheroof.com>
2015-06-29 16:50:34 +0000

    update fitvids plugin to automatically resize all videos within text modules

--- js/site.js
+++ js/site.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	$('.form-horizontal .control-group, .form-comments .control-group').addClass('form-group');
 	$('.form-group select, .form-group textarea, .form-group input').addClass('form-control');
 	$('.comments, .field.field-tags').addClass('well');
-	
+
 	//Add Bootstrap Dropdowns to Nav
 	$('.nav li.has-children ul.child-menu').each(function() {
 		var copyThis = $(this).siblings('a');
@@ -22,10 +22,10 @@ $(document).ready(function() {
 		e.stopPropagation(); 
 		$('ul.dropdown-menu [data-toggle=dropdown]').parent().removeClass('open');
 		$(this).parent().addClass('open');
-	
+
 		var menu = $(this).parent().find("ul");
 		var menupos = $(menu).offset();
-	
+
 		if (menupos.left + menu.width() > $(window).width()) {
 			var newpos = -$(menu).width();
 			menu.css({ left: newpos });    
@@ -75,83 +75,83 @@ $(document).ready(function() {
 
 (function($){
 
-  "use strict";
+	"use strict";
 
-  $.fn.fitVids = function( options ) {
-    var settings = {
-      customSelector: null
-    };
+	$.fn.fitVids = function( options ) {
+		var settings = {
+			customSelector: null
+		};
 
-    if(!document.getElementById('fit-vids-style')) {
+		if(!document.getElementById('fit-vids-style')) {
 
-      var div = document.createElement('div'),
-          ref = document.getElementsByTagName('base')[0] || document.getElementsByTagName('script')[0];
+			var div = document.createElement('div'),
+				ref = document.getElementsByTagName('base')[0] || document.getElementsByTagName('script')[0];
 
-      div.className = 'fit-vids-style';
-      div.id = 'fit-vids-style';
-      div.style.display = 'none';
-      div.innerHTML = '&shy;<style>         \
-        .fluid-width-video-wrapper {        \
-           width: 100%;                     \
-           position: relative;              \
-           padding: 0;                      \
-        }                                   \
-                                            \
-        .fluid-width-video-wrapper iframe,  \
-        .fluid-width-video-wrapper object,  \
-        .fluid-width-video-wrapper embed {  \
-           position: absolute;              \
-           top: 0;                          \
-           left: 0;                         \
-           width: 100%;                     \
-           height: 100%;                    \
-        }                                   \
-      </style>';
+			div.className = 'fit-vids-style';
+			div.id = 'fit-vids-style';
+			div.style.display = 'none';
+			div.innerHTML = '&shy;<style>         \
+.fluid-width-video-wrapper {        \
+width: 100%;                     \
+position: relative;              \
+padding: 0;                      \
+}                                   \
+\
+.fluid-width-video-wrapper iframe,  \
+.fluid-width-video-wrapper object,  \
+.fluid-width-video-wrapper embed {  \
+position: absolute;              \
+top: 0;                          \
+left: 0;                         \
+width: 100%;                     \
+height: 100%;                    \
+}                                   \
+</style>';
 
-      ref.parentNode.insertBefore(div,ref);
+			ref.parentNode.insertBefore(div,ref);
 
-    }
+		}
 
-    if ( options ) {
-      $.extend( settings, options );
-    }
+		if ( options ) {
+			$.extend( settings, options );
+		}
 
-    return this.each(function(){
-      var selectors = [
-        "iframe[src*='player.vimeo.com']",
-        "iframe[src*='youtube.com']",
-        "iframe[src*='youtube-nocookie.com']",
-        "iframe[src*='kickstarter.com'][src*='video.html']",
-        "object",
-        "embed"
-      ];
+		return this.each(function(){
+			var selectors = [
+				"iframe[src*='player.vimeo.com']",
+				"iframe[src*='youtube.com']",
+				"iframe[src*='youtube-nocookie.com']",
+				"iframe[src*='kickstarter.com'][src*='video.html']",
+				"object",
+				"embed"
+			];
 
-      if (settings.customSelector) {
-        selectors.push(settings.customSelector);
-      }
+			if (settings.customSelector) {
+				selectors.push(settings.customSelector);
+			}
 
-      var $allVideos = $(this).find(selectors.join(','));
-      $allVideos = $allVideos.not("object object"); // SwfObj conflict patch
+			var $allVideos = $(this).find(selectors.join(','));
+			$allVideos = $allVideos.not("object object"); // SwfObj conflict patch
 
-      $allVideos.each(function(){
-        var $this = $(this);
-        if (this.tagName.toLowerCase() === 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; }
-        var height = ( this.tagName.toLowerCase() === 'object' || ($this.attr('height') && !isNaN(parseInt($this.attr('height'), 10))) ) ? parseInt($this.attr('height'), 10) : $this.height(),
-            width = !isNaN(parseInt($this.attr('width'), 10)) ? parseInt($this.attr('width'), 10) : $this.width(),
-            aspectRatio = height / width;
-        if(!$this.attr('id')){
-          var videoID = 'fitvid' + Math.floor(Math.random()*999999);
-          $this.attr('id', videoID);
-        }
-        $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+"%");
-        $this.removeAttr('height').removeAttr('width');
-      });
-    });
-  };
+			$allVideos.each(function(){
+				var $this = $(this);
+				if (this.tagName.toLowerCase() === 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; }
+				var height = ( this.tagName.toLowerCase() === 'object' || ($this.attr('height') && !isNaN(parseInt($this.attr('height'), 10))) ) ? parseInt($this.attr('height'), 10) : $this.height(),
+					width = !isNaN(parseInt($this.attr('width'), 10)) ? parseInt($this.attr('width'), 10) : $this.width(),
+					aspectRatio = height / width;
+				if(!$this.attr('id')){
+					var videoID = 'fitvid' + Math.floor(Math.random()*999999);
+					$this.attr('id', videoID);
+				}
+				$this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+"%");
+				$this.removeAttr('height').removeAttr('width');
+			});
+		});
+	};
 })( jQuery );
 
 
 $(document).ready(function(){
-// fit those vids
-$("#content").fitVids({ customSelector: "iframe[src^='http://www.screenr.com']"});
+	// fit those vids
+	$(".bit-text").fitVids({ customSelector: "iframe[src^='http://www.screenr.com']"});
 });
